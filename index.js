c8e59b02a5703e37f93c36e7d06442bba1d29279
Joe Weakley <joew@samjoe.com>
2014-12-13 13:55:50 -0800

    Added chat.

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+  res.render("page");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+  res.send("It works!");
+});
+var io = require('socket.io').listen(app.listen(port));
+
+console.log("Listening on port " + port);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { message: 'Welcome to the chat' });
+  socket.on('send', function (data) {
+    //console.log(data.message);
+    io.sockets.emit('message', data);
+  });
+});
